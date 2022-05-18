e19d937cf17177cfce7c0f9cd3048bce0381ee5b
Hassan Rabbi <hassan_rabbi@live.com>
2022-05-18 13:21:06 +0600

    check authorization header on incoming request

---
+++ middlewares/auth/isAuth.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authorization = req.get("Authorization");
+  if (!authorization) {
+    const error = new Error("No token found.");
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authorization.split(" ")[1];
+
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  } catch (err) {
+    err.statusCode = 500;
+    next(err);
+  }
+
+  if (!decodedToken) {
+    const error = new Error("Not authorized!");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  req.userId = decodedToken.userId;
+  next();
+};
